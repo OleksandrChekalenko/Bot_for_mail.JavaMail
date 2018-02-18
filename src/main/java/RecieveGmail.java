41c83f5b07553e61b864bac03b819f77a641c55d
Alexandr Chekalenko <whibleykevin@gmail.com>
2018-02-18 20:11:22 +0200

    Auto answer message

--- src/main/java/RecieveGmail.java
+++ src/main/java/RecieveGmail.java
@@ -1,109 +1,135 @@
 import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
 import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 public class RecieveGmail {
 
     private static String saveDirectory = "D:/Downloads/Attachments";
 
+    public void downloadAttachment(Message message) throws MessagingException, IOException {
+        String contentType = message.getContentType();
+        String messageContent = "";
 
-    public static void check(String host, String storeType, String user,
-                             String password)
-{
-    try {
+        // store attachment file name, separated by comma
+        String attachFiles = "";
 
-        //create properties field
-        Properties properties = new Properties();
-
-        properties.put("mail.pop3.host", host);
-        properties.put("mail.pop3.port", "995");
-        properties.put("mail.pop3.starttls.enable", "true");
-        Session emailSession = Session.getDefaultInstance(properties);
-
-        //create the POP3 store object and connect with the pop server
-        Store store = emailSession.getStore("pop3s");
-
-        store.connect(host, user, password);
-
-        //create the folder object and open it
-        Folder emailFolder = store.getFolder("INBOX");
-        emailFolder.open(Folder.READ_ONLY);
-
-        // retrieve the messages from the folder in an array and print it
-        Message[] messages = emailFolder.getMessages();
-        System.out.println("messages.length---" + messages.length);
-
-//        for (int i = 0, n = messages.length; i < 1; i++) {
-        for (int i = messages.length-1 ; i == messages.length-1; i++) {
-
-            Message message = messages[i];
-            System.out.println("---------------------------------");
-            System.out.println("Email Number " + (i + 1));
-            System.out.println("Subject: " + message.getSubject());
-            System.out.println("From: " + message.getFrom()[0]);
-            System.out.println("Text: " + message.getContent().toString());
-            System.out.println(message.getContent());
-
-            String contentType = message.getContentType();
-            String messageContent = "";
-
-            // store attachment file name, separated by comma
-            String attachFiles = "";
-
-            if (contentType.contains("multipart")) {
-                // content may contain attachments
-                Multipart multiPart = (Multipart) message.getContent();
-                int numberOfParts = multiPart.getCount();
-                for (int partCount = 0; partCount < numberOfParts; partCount++) {
-                    MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
-                    if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
-                        // this part is attachment
-                        String fileName = part.getFileName();
-                        attachFiles += fileName + ", ";
-                        part.saveFile(saveDirectory + File.separator + fileName);
-                    } else {
-                        // this part may be the message content
-                        messageContent = part.getContent().toString();
-                    }
-                }
-
-                if (attachFiles.length() > 1) {
-                    attachFiles = attachFiles.substring(0, attachFiles.length() - 2);
-                }
-            } else if (contentType.contains("text/plain")
-                    || contentType.contains("text/html")) {
-                Object content = message.getContent();
-                if (content != null) {
-                    messageContent = content.toString();
+        if (contentType.contains("multipart")) {
+            // content may contain attachments
+            Multipart multiPart = (Multipart) message.getContent();
+            int numberOfParts = multiPart.getCount();
+            for (int partCount = 0; partCount < numberOfParts; partCount++) {
+                MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
+                if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+                    // this part is attachment
+                    String fileName = part.getFileName();
+                    attachFiles += fileName + ", ";
+                    part.saveFile(saveDirectory + File.separator + fileName);
+                } else {
+                    // this part may be the message content
+                    messageContent = part.getContent().toString();
                 }
             }
 
+            if (attachFiles.length() > 1) {
+                attachFiles = attachFiles.substring(0, attachFiles.length() - 2);
+            }
+        } else if (contentType.contains("text/plain")
+                || contentType.contains("text/html")) {
+            Object content = message.getContent();
+            if (content != null) {
+                messageContent = content.toString();
+            }
+        }
+    }
+
+    public void sent(Session session) throws MessagingException {
+
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress("nature2578@gmail.com"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("whibleykevin@gmail.com"));
+            message.setSubject("Test");
+            message.setText("Bla");
+
+            Transport.send(message);
+
+            /*Transport transport = new session.getTransport("smtp");
+            transport.connect("smtp.gmail.com","nature2578","srtsrvts123");
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();*/
 
 
+        } catch (AddressException e) {
+            e.getMessage();
+        } catch (MessagingException e) {
+            e.getMessage();
         }
 
-        //close the store and folder objects
-        emailFolder.close(false);
-        store.close();
 
-    } catch (NoSuchProviderException e) {
-        e.printStackTrace();
-    } catch (MessagingException e) {
-        e.printStackTrace();
-    } catch (Exception e) {
-        e.printStackTrace();
+        System.out.println("Sent autoAnswer successfully......");
     }
-}
+
+    public void read() {
+
+        Properties props = new Properties();
+
+        try {
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("nature2578@gmail.com",
+                            "srtsrvts123");
+                }
+            });
+
+            // props.load(new FileInputStream(new File("C:\\Users\\Cagy\\IdeaProjects\\chelexmail4\\smtp.properties")));
+            //Session session = Session.getDefaultInstance(props);
+
+            Store store = session.getStore("imaps");
+            store.connect("smtp.gmail.com", "nature2578", "srtsrvts123");
+
+            Folder inbox = store.getFolder("inbox");
+            inbox.open(Folder.READ_ONLY);
+            int messageCount = inbox.getMessageCount();
+
+            System.out.println("Total Messages:- " + messageCount);
+
+            Message[] messages = inbox.getMessages();
+            System.out.println("------------------------------");
+
+
+            Message message = messages[messages.length - 1];
+            System.out.println("Email Number " + (messages.length));
+            System.out.println("Subject: " + message.getSubject());
+            System.out.println("From: " + message.getFrom()[0]);
+            System.out.println("Text: " + message.getContent().toString());
+
+            downloadAttachment(message);
+            sent(session);
+
+            inbox.close(true);
+            store.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
 
-        String host = "pop.gmail.com";// change accordingly
-        String mailStoreType = "pop3";
-        String username = "nature2578@gmail.com";// change accordingly
-        String password = "srtsrvts123";// change accordingly
-
-        check(host, mailStoreType, username, password);
+       RecieveGmail gmail = new RecieveGmail();
+        gmail.read();
 
     }
 }
